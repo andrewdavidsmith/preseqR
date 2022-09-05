724fd0e1c5904e60a631e94930bed39f69279e12
Andrew D Smith <andrewds@usc.edu>
2022-09-05 15:34:03 -0700

    Updating the data files and raw data files

--- data-raw/make_data.R
+++ data-raw/make_data.R
@@ -1,2 +1,35 @@
+Dickens <- read.table('Dickens.txt', header=T)
+save(Dickens, file='../data/Dickens.rda')
+
+FisherButterfly <- read.table('FisherButterfly.txt', header=T)
+save(FisherButterfly, file='../data/FisherButterfly.rda')
+
+SRR061157_k31 <- read.table('SRR061157_k31.txt', header=T)
+save(SRR061157_k31, file='../data/SRR061157_k31.rda')
+
+SRR1301329_1M_base <- read.table('SRR1301329_1M_base.txt', header=T)
+save(SRR1301329_1M_base, file='../data/SRR1301329_1M_base.rda')
+
+SRR1301329_1M_read <- read.table('SRR1301329_1M_read.txt', header=T)
+save(SRR1301329_1M_read, file='../data/SRR1301329_1M_read.rda')
+
+SRR1301329_base <- read.table('SRR1301329_base.txt', header=T)
+save(SRR1301329_base, file='../data/SRR1301329_base.rda')
+
+SRR1301329_read <- read.table('SRR1301329_read.txt', header=T)
+save(SRR1301329_read, file='../data/SRR1301329_read.rda')
+
+SRR611492 <- read.table('SRR611492.txt', header=T)
+save(SRR611492, file='../data/SRR611492.rda')
+
+SRR611492_5M <- read.table('SRR611492_5M.txt', header=T)
+save(SRR611492_5M, file='../data/SRR611492_5M.rda')
+
 Shakespeare <- read.table('Shakespeare.txt', header=T)
 save(Shakespeare, file='../data/Shakespeare.rda')
+
+Twitter <- read.table('Twitter.txt', header=T)
+save(Twitter, file='../data/Twitter.rda')
+
+WillButterfly <- read.table('WillButterfly.txt', header=T)
+save(WillButterfly, file='../data/WillButterfly.rda')
